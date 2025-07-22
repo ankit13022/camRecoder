7e6ab44ebc8f4ef8313725e608b45c1dad4267cc
Ankit <ankitdj13112002@gmail.com>
2025-07-22 11:26:40 +0530

    change in style

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import CameraRecorder from "../components/camRecoder";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen p-5 flex-col items-center justify-center bg-gray-800">
+    <main className="flex min-h-screen p-10 flex-col items-center justify-center bg-gray-800">
       <h1 className="text-3xl font-bold mb-6">Face Tracking Recorder</h1>
       <p className="text-lg mb-4">
         Record your face with real-time tracking and download your video.
